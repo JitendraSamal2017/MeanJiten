0665d3542dde391de73d73f320cf155892c522e0
JitendraSamal2017 <jitendrak.samal@gmail.com>
2017-06-20 07:27:23 +0000

    changes in first

--- server.js
+++ server.js
@@ -3,9 +3,19 @@ var bodyParser = require('body-parser');
 var mongoose=require('mongoose');
 var app = express();
 app.use(bodyParser.json());
+app.use(express.static('public')); 
 mongoose.connect('mongodb://localhost:27017/mittens');
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
+});
+
+var Meow = mongoose.model('Meow', {
+ text: String
+  });
+app.get('/meows',function(req,res,next){
+  Meow.find({}, function (err,meows){
+    return res.json(meows);
+  });
 });
 
